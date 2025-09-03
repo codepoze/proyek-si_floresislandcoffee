f5c56d5b6a0ab4c96da70101a1c0d605dc7e7ced
Alan Lengkoan <alansaputralengkoan@gmail.com>
2025-09-03 10:37:39 +0800

    update BE

--- BE/resources/views/admin/components/admin-sidebar.blade.php
+++ BE/resources/views/admin/components/admin-sidebar.blade.php
@@ -19,79 +19,52 @@
     <div class="shadow-bottom"></div>
     <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li class="nav-item {{ request()->is('admin/dashboard') ? 'active' : ''  }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.dashboard.index') }}">
+            <li class="nav-item">
+                <a class="d-flex align-items-center" href="#">
                     <i data-feather="home"></i><span class="menu-title text-truncate">Dashboard</span>
                 </a>
             </li>
-            
-            <li class="nav-item {{ request()->is('admin/distributor') ? 'active' : ''  }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.distributor.index') }}">
-                    <i data-feather="truck"></i><span class="menu-title text-truncate">Distributor</span>
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a class="d-flex align-items-center" href="#">
-                    <i data-feather="list"></i><span class="menu-title text-truncate">Master Produk</span>
-                </a>
-                <ul class="menu-content">
-                    <li>
-                        <a class="d-flex align-items-center" href="{{ route('admin.satuan.index') }}">
-                            <i data-feather="tag"></i><span class="menu-item text-truncate">Satuan</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="d-flex align-items-center" href="{{ route('admin.produk.index') }}">
-                            <i data-feather="package"></i><span class="menu-item text-truncate">Produk</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="nav-item">
-                <a class="d-flex align-items-center" href="#">
-                    <i data-feather="list"></i><span class="menu-title text-truncate">Master Antrean</span>
-                </a>
-                <ul class="menu-content">
-                    <li>
-                        <a class="d-flex align-items-center" href="{{ route('admin.slider.index') }}">
-                            <i data-feather="image"></i><span class="menu-item text-truncate">Slider</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="d-flex align-items-center" href="{{ route('admin.metode.index') }}">
-                            <i data-feather="settings"></i><span class="menu-item text-truncate">Metode</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
 
             <li class="navigation-header">
-                <span>Proses Antrean</span><i data-feather="more-horizontal"></i>
+                <span>Master</span><i data-feather="more-horizontal"></i>
             </li>
-            <li class="nav-item {{ request()->is('admin/pendaftaran') ? 'active' : '' }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.pendaftaran.index') }}">
-                    <i data-feather="edit-3"></i><span class="menu-title text-truncate">Pendaftaran</span>
+            <li class="nav-item">
+                <a class="d-flex align-items-center" href="#">
+                    <i data-feather="list"></i><span class="menu-title text-truncate">Slider</span>
                 </a>
             </li>
-            <li class="nav-item {{ request()->is('admin/antrean') ? 'active' : '' }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.antrean.index') }}">
-                    <i data-feather="clock"></i><span class="menu-title text-truncate">Antrean</span>
+            <li class="nav-item">
+                <a class="d-flex align-items-center" href="#">
+                    <i data-feather="list"></i><span class="menu-title text-truncate">Sosial Media</span>
                 </a>
             </li>
-            <li class="nav-item {{ request()->is('admin/display') ? 'active' : '' }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.display.index') }}" target="_blank">
-                    <i data-feather="monitor"></i><span class="menu-title text-truncate">Display</span>
+            <li class="nav-item">
+                <a class="d-flex align-items-center" href="#">
+                    <i data-feather="list"></i><span class="menu-title text-truncate">Pengunjung</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="d-flex align-items-center" href="#">
+                    <i data-feather="list"></i><span class="menu-title text-truncate">Kontak</span>
                 </a>
             </li>
 
             <li class="navigation-header">
-                <span>Laporan</span><i data-feather="more-horizontal"></i>
+                <span>Blog</span><i data-feather="more-horizontal"></i>
             </li>
-            <li class="nav-item {{ request()->is('admin/laporan/antrean') ? 'active' : '' }}">
-                <a class="d-flex align-items-center" href="{{ route('admin.laporan.antrean') }}">
-                    <i data-feather="file-text"></i><span class="menu-title text-truncate">Antrean</span>
+            <li class="nav-item">
+                <a class="d-flex align-items-center" href="#">
+                    <i data-feather="list"></i><span class="menu-title text-truncate">Kategori</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="d-flex align-items-center" href="#">
+                    <i data-feather="list"></i><span class="menu-title text-truncate">Tag</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="d-flex align-items-center" href="#">
+                    <i data-feather="list"></i><span class="menu-title text-truncate">Post</span>
                 </a>
             </li>
 
--- BE/resources/views/admin/dashboard/view.blade.php
+++ BE/resources/views/admin/dashboard/view.blade.php
@@ -7,16 +7,7 @@
     <!-- begin:: content -->
     <section>
         <div class="row justify-content-center">
-            @foreach ($antrean as $row)
-            <div class="col-md-4 col-6 mb-3">
-                <div class="card text-center">
-                    <div class="card-body">
-                        <h4>{{ $row['no_antrean'] }}</h4>
-                        <h6 class="text-muted m-b-0">Jumlah Antrean {{ $row['nama'] }}</h6>
-                    </div>
-                </div>
-            </div>
-            @endforeach
+            <h2>Selamat Datang di Sistem Informasi</h2>
         </div>
     </section>
 
